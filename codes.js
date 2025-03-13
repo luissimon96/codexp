6f13a138b94ad72c2e3d0d24a4c9839e3dac1695
gabrielgondev <gabrielgoncalvesdoliveira@gmail.com>
2025-03-12 21:24:47 -0300

    Implement code registration feature with user authentication and dynamic code display

---
+++ codes.js
@@ -0,0 +1,15 @@
+const codes = [
+    { code: "ADMIN_STRENGTH", reward: "20 minutes of 2x XP" },
+    { code: "SUB2GAMERROBOT_RESET1", reward: "Stat Reset" },
+    { code: "SUB2GAMERROBOT_EXP1", reward: "30 minutes of 2x XP" },
+    { code: "SUB2NOOBMASTER123", reward: "15 minutes of 2x XP" },
+    { code: "SUB2UNCLEKIZARU", reward: "Stat Reset" },
+    { code: "SUB2DAIGROCK", reward: "15 minutes of 2x XP" },
+    { code: "AXIORE", reward: "20 minutes of 2x XP" },
+    { code: "TANTAI_GAMING", reward: "15 minutes of 2x XP" },
+    { code: "STRAWHATMAINE", reward: "15 minutes of 2x XP" },
+    { code: "UPD16", reward: "20 minutes of 2x XP" },
+    { code: "FUDD10", reward: "$1" },
+    { code: "BIGNEWS", reward: "In-game title" },
+    { code: "THEGREATACE", reward: "20 minutes of 2x XP" }
+];
