fbc00a389e00eeec47f3d8636334ea7f1b88339c
Luis Eduardo Simon <luissimon96@gmail.com>
2025-03-12 23:52:41 -0300

    Atualiza o manifesto e estrutura do projeto Codexp, incluindo a reorganização de caminhos para ícones e arquivos, adição de novas páginas (cadastro, login, geração de ícones), implementação de um sistema de gerenciamento de códigos, e suporte a PWA com service worker. Também foram adicionados estilos para melhorar a responsividade e a experiência do usuário.

--- src/js/pwa.js
+++ src/js/pwa.js
@@ -6,7 +6,7 @@ const installButton = document.getElementById('install-button');
 // Registrar o service worker
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('service-worker.js')
+        navigator.serviceWorker.register('../js/service-worker.js')
             .then(registration => {
                 console.log('Service Worker registrado com sucesso:', registration);
             })
